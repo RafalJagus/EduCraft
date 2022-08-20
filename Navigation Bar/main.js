6bf67eff56a9636f8277718f2315a326f21d9c53
Rafał Jaguś <94844926+RafalJagus@users.noreply.github.com>
2022-08-20 15:36:14 +0200

    Add files via upload

---
+++ Navigation Bar/main.js
@@ -0,0 +1,12 @@
+let btn = document.querySelector("#btn");
+let sidebar = document.querySelector(".sidebar");
+let searchBtn = document.querySelector(".bx-search");
+
+btn.onclick =   function(){
+    sidebar.classList.toggle("open")
+
+}
+searchBtn.onclick =   function(){
+    sidebar.classList.toggle("open")
+
+}
