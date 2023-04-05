a48bb8870ec5288963211421e548264a7f7f6dcd
Rafał Jaguś <94844926+RafalJagus@users.noreply.github.com>
2023-04-06 00:39:27 +0200

    Implementacja kodu 0.1
    add class Thesaurus
    add public variable thesaurus stores words
    add Konstruktor

---
+++ zadanie rekrutacyjne/php 4/index.php
@@ -0,0 +1,9 @@
+<?php
+
+class Thesaurus {
+    public $thesaurus; //przechowuje słowa
+
+    public function __construct($thesaurus) { //konstruktor
+        $this->thesaurus = $thesaurus;
+    }
+?>
