1a9c52991c0192195e63ccddd4d029b9d6dbde0e
Rafał Jaguś <94844926+RafalJagus@users.noreply.github.com>
2023-04-05 20:57:48 +0200

    Implementacja kodu php
    -Add class Pipeline
    - Add make metod 
    - Add function code 
    - Add function call according to the guidelines 
    make(function($var) { return $var * 3; }, function($var) { return $var + 1; },
    function($var) { return $var / 2; }),

--- zadanie rekrutacyjne/php/index.php
+++ zadanie rekrutacyjne/php/index.php
@@ -1,12 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Zadanie Rekrutacyjne</title>
 </head>
 <body>
-    
+    <h1>hello world!</h1>
+    <?php
+
+//klasa Pipeline
+class Pipeline
+{
+    public static function make($functions)
+    {
+        return function ($arg) use ($functions) {  
+            foreach ($functions as $function) {
+                $result = $function($arg);
+            }
+            return $arg;
+        };
+    }
+}
+
+// przykładowe uzycie metody zawarte w zadaniu
+$pipeline = Pipeline::make(
+    function($var) { return $var * 3; },
+    function($var) { return $var + 1; },
+    function($var) { return $var / 2; }
+);
+// wywołanie 
+echo $pipeline(3);
+
+
+    ?>
 </body>
 </html>
