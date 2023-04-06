108ee2894a733570d6fed96f6b8909e060de5c58
Rafał Jaguś <94844926+RafalJagus@users.noreply.github.com>
2023-04-06 08:15:37 +0200

    Implementacja kodu
    add getSynonyms - returns the synonym of the given word
    add dictionary with "market" and "small" 
    -the program runs and shows the word query along with synonyms

--- zadanie rekrutacyjne/php 4/index.php
+++ zadanie rekrutacyjne/php 4/index.php
@@ -6,4 +6,27 @@ class Thesaurus {
     public function __construct($thesaurus) { //konstruktor
         $this->thesaurus = $thesaurus;
     }
+
+    public function getSynonyms($word) {  // zwraca synonimy dla podanego słowa
+        if (isset($this->thesaurus[$word])) {
+            $synonyms = $this->thesaurus[$word];
+        } else {
+            $synonyms = array();
+        }
+
+        $result = array(
+            "word" => $word,
+            "synonyms" => $synonyms
+        );
+
+        return json_encode($result); // zwraca tablicę  
+    }
+}
+$thesaurus = new Thesaurus(array( // słownik
+    "market" => array("trade"),
+    "small" => array("little", "compact")
+));
+
+echo $thesaurus->getSynonyms("small"); 
+echo $thesaurus->getSynonyms("asleast");
 ?>
